8342a280262adab28218ef8b8df9118d0c2570ee
Botita <47467914+Botitaa@users.noreply.github.com>
2026-08-27 17:06:20 -0300

    Refactor PersonajeJuego to richer character model
    Replace simple elegido flag with a richer immutable character model. Add imports for Genero and ColorPelo, make id and nombre final, and introduce genero, esCalvo, usaLentes and colorPelo fields with an expanded constructor. Provide getters (getGenero, esCalvo, usaLentes, getColorPelo). Remove the elegido field and related methods (elegir, esElegido) and the previous toString. This refactor prepares PersonajeJuego for attribute-based querying and game logic. Note: this is a breaking API change for Personaje consumers that relied on the old chosen flag or toString.

--- src/dominio/PersonajeJuego.java
+++ src/dominio/PersonajeJuego.java
@@ -1,15 +1,24 @@
 package dominio;
 
-public class PersonajeJuego implements Personaje {
+import valores.ColorPelo;
+import valores.Genero;
 
-    private int id;
-    private String nombre;
-    private boolean elegido;
+public class PersonajeJuego implements Personaje{
 
-    public PersonajeJuego(int id, String nombre){
+    private final int id;
+    private final String nombre;
+    private final Genero genero;
+    private final boolean esCalvo;
+    private final boolean usaLentes;
+    private final ColorPelo colorPelo;
+
+    public PersonajeJuego(int id, String nombre, Genero genero, boolean esCalvo, boolean usaLentes, ColorPelo colorPelo) {
         this.id = id;
         this.nombre = nombre;
-        this.elegido = false;
+        this.genero = genero;
+        this.esCalvo = esCalvo;
+        this.usaLentes = usaLentes;
+        this.colorPelo = colorPelo;
     }
 
     @Override
@@ -23,17 +32,22 @@ public class PersonajeJuego implements Personaje {
     }
 
     @Override
-    public boolean esElegido() {
-        return this.elegido;
+    public Genero getGenero() {
+        return this.genero;
     }
 
     @Override
-    public void elegir() {
-        this.elegido = true;
+    public boolean esCalvo() {
+        return this.esCalvo;
     }
 
     @Override
-    public String toString() {
-        return "ID: " + id + " | Nombre: " + nombre + " | Elegido: " + elegido;
+    public boolean usaLentes() {
+        return this.usaLentes;
+    }
+
+    @Override
+    public ColorPelo getColorPelo() {
+        return this.colorPelo;
     }
 }
