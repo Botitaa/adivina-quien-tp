3d85f1a83b65f996c1a8a799765423d618bad48c
Marcos Enevoldsen <marcosenevoldsen@gmail.com>
2026-08-27 16:20:42 -0300

    eliminacion de interface e implementaciones

--- src/interfaces/Personaje.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface Personaje {
-    int getId(); //devuelve el id
-    String getNombre(); // devuelve el nombre del personaje
-    boolean esElegido(); // devuelve si es o no elegido
-    void elegir(); //setea el personaje a adivinar
-    String toString(); // documenta que toda implementación debe dar una representación legible
-}
